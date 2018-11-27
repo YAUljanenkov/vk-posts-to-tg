f1f915a3f2e823d1f72b6ce6eaef7193e2de2c34
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2018-11-28 00:16:07 +0300

    Alpha version

---
+++ README.md
@@ -0,0 +1,31 @@
+# VK posts->Tg
+*Transfer your VK posts to telegram immediately*
+
+> **Warning!** This is an alpha version and is **not** tested well.
+> It ~~might be~~ *is* not stable.
+
+This bot uses VK long poll API to get notifications about posts on-time and resend them in telegram channel.
+Right now bot can resend text, photos and docs from your post. Soon new options and better configuration will be added.
+Also soon I'll make code prettier.
+
+## How to configure this bot
+
+1. Visit vk.com/<your_group_id>?act=tokens and create a token with "wall" rights.
+2. Secodly, go to Telegram and create a bot using `@BotFather`
+3. Add your bot to administrators of your channel
+4. Clone this repo
+5. Create file `config.py` and fill it using this template:
+    ```angular2html
+    VK_TOKEN = '<YOUR TOKEN>'
+    VK_GROUP_ID = '<YOUR VK GROUP ID'
+    TELEGRAM_GROUP_ID = '<ID OF YOUR Tg CHANNEL>'
+    TELEGRAM_BOT_TOKEN = '<BOT TOKEN>'
+    ```
+    I recommend you to use `@getidsbot` to get your telegram channel ID. 
+6. Run `pip3 install -r requirements.txt`
+7. Run `python3 main.py`
+8. ...
+9. Profit!
+
+
+
---
+++ main.py
@@ -0,0 +1,48 @@
+import sys
+import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from config import VK_TOKEN, VK_GROUP_ID, TELEGRAM_BOT_TOKEN, TELEGRAM_GROUP_ID
+import telegram
+from telegram.ext import Updater
+import logging
+
+logging.basicConfig(format=u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level=logging.INFO)
+
+
+def main():
+    updater = Updater(TELEGRAM_BOT_TOKEN)
+    bot = updater.bot
+    vk_session = vk_api.VkApi(token=VK_TOKEN)
+
+    long_poll = VkBotLongPoll(vk_session, VK_GROUP_ID)
+
+    try:  # TODO: rewrite the code
+
+        for event in long_poll.listen():
+
+            if event.type == VkBotEventType.WALL_POST_NEW:
+                logging.info('New post received')
+                if event.obj.text != '':
+                    bot.send_message(TELEGRAM_GROUP_ID, text=event.obj.text)
+                if 'attachments' in event.obj.keys():
+                    for attachment in event.obj['attachments']:
+                        if attachment['type'] == 'photo':
+                            for photo_size in ['photo_2560', 'photo_1280', 'photo_807', 'photo_604', 'photo_130']:
+                                try:
+                                    bot.send_photo(TELEGRAM_GROUP_ID, attachment['photo'][photo_size])
+                                    break
+                                except KeyError:
+                                    continue
+                        if attachment['type'] == 'doc':
+                            bot.send_document(TELEGRAM_GROUP_ID, attachment['doc']['url'])
+    except telegram.error.TimedOut:
+        logging.error('Timeout Error in tg lib')
+
+    except:
+        logging.error("Unexpected error: {}".format(sys.exc_info()[0]))
+        pass
+
+
+if __name__ == '__main__':
+    while True:
+        main()
